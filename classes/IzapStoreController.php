b084f4fa3a4fc900f5c2f53da9f372046d9cc6c3
Carl <carl@izap.in>
2001-01-13 04:00:25 +0000

    replaced the depricated functions with new one

--- classes/IzapStoreController.php
+++ classes/IzapStoreController.php
@@ -173,8 +173,8 @@ class IzapStoreController extends IzapController {
       $contents = file_get_contents($IZAP_ECOMMERCE->default_image);
     }
 
-    $file_friendly_name = friendly_title($izap_product->title . '.' . $izap_product->image_extension);
-    izap_cache_headers(array(
+    $file_friendly_name = elgg_get_friendly_title($izap_product->title . '.' . $izap_product->image_extension);
+    IzapBase::izapCacheHeaders(array(
         'content_type' => $izap_product->image_mime_type,
         'file_name' => $file_friendly_name,
         'filemtime' => filemtime($file_name),
