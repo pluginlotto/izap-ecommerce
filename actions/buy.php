9eef570e3ebcaa4dce3c2a0642b043455212b21f
Carl <carl@izap.in>
2001-08-08 22:38:58 +0000

    Updates

--- actions/buy.php
+++ actions/buy.php
@@ -11,21 +11,20 @@
 * For more information. Contact "Tarun Jangra<tarun@izap.in>"
 * For discussion about corresponding plugins, visit http://www.pluginlotto.com/pg/forums/
 * Follow us on http://facebook.com/PluginLotto and http://twitter.com/PluginLotto
-*/
+ */
 
-include_once dirname(dirname(__FILE__)) . '/lib/gateways/paypal/paypal.php';
-include_once dirname(dirname(__FILE__)) . '/lib/gateways/clsGateway.php';
+//include_once dirname(dirname(__FILE__)) . '/lib/gateways/paypal/paypal.php';
+//include_once dirname(dirname(__FILE__)) . '/lib/gateways/clsGateway.php';
 
 global $IZAP_ECOMMERCE;
 $cart_id = time();
 $cart = get_from_session_izap_ecommerce('izap_cart');
 
-$data_array['loginId'] = get_plugin_setting('paypal_account', $IZAP_ECOMMERCE->plugin_name);
-
+$data_array['business'] = get_plugin_setting('paypal_account', $IZAP_ECOMMERCE->plugin_name);
 $data_array['items'] = get_from_session_izap_ecommerce('items');
 $data_array['grandTotal'] = get_from_session_izap_ecommerce('total_cart_price');
 $data_array['return'] = $IZAP_ECOMMERCE->link . 'pay_return';
-$data_array['notifyUrl'] = $IZAP_ECOMMERCE->link . 'paypal_notify';
+$data_array['notify_url'] = $IZAP_ECOMMERCE->link . 'paypal_notify';
 
 $debug = FALSE;
 if(get_plugin_setting('sandbox', $IZAP_ECOMMERCE->plugin_name) == 'yes') {
@@ -36,8 +35,11 @@ $guid = save_order_izap_ecommerce($data_array['items'], $cart_id);
 if($guid) {
   add_to_session_izap_ecommerce('cart_id', $cart_id);
   $data_array['custom'] = $guid;
-  $gateway = new gateway('paypal', '', $debug);
-  $gateway->paypal($data_array);
+//  $gateway = new gateway('paypal', '', $debug);
+//  $gateway->paypal($data_array);
+  $payment = new IzapPayment('paypal', $debug);
+  $payment->setParams($data_array);
+  $payment->process();
 }else {
   register_error(__('unable_to_save_order'));
   forward($_SERVER['HTTP_REFERER']);
