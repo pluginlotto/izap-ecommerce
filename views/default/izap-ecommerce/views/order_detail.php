f772051952292c0a465ca0e9fed706b6e3ee99f9
Carl <carl@izap.in>
2001-08-19 01:58:04 +0000

    Updates for order detai;l, listing, etc

--- views/default/izap-ecommerce/views/order_detail.php
+++ views/default/izap-ecommerce/views/order_detail.php
@@ -11,11 +11,85 @@
 * For more information. Contact "Tarun Jangra<tarun@izap.in>"
 * For discussion about corresponding plugins, visit http://www.pluginlotto.com/pg/forums/
 * Follow us on http://facebook.com/PluginLotto and http://twitter.com/PluginLotto
-*/
+ */
 
 global $IZAP_ECOMMERCE;
 $order = $vars['entity'];
+$order_owner = $order->getOwnerEntity();
+if(is_plugin_enabled('messages')) {
+  $notify_link = $vars['url'] . 'mod/messages/send.php?send_to=' . $order_owner->guid;
+}else{
+  $notify_link = 'mailto:' . $order_owner->email;
+}
+?>
 
+<div class="contentWrapper">
+  <div class="order_detail_owner">
+
+    <div class="icon">
+      <a href="<?php echo $order_owner->getURL();?>">
+        <?php
+        echo elgg_view('profile/icon', array('entity' => $order_owner, 'override' => true));
+        ?>
+      </a>
+    </div>
+
+    <div class="info">
+      <?php echo elgg_echo('izap-ecommerce:name'); ?>:
+      <b>
+        <a href="<?php echo $order_owner->getURL();?>">
+          <?php echo $order_owner->name;?>
+        </a>
+      </b>
+      <br />
+
+      <?php echo elgg_echo('izap-ecommerce:email'); ?>:
+      <b>
+        <a href="<?php echo $notify_link;?>">
+          <?php echo $order_owner->email;?>
+        </a>
+      </b>
+      <br />
+
+      <?php echo elgg_echo('izap-ecommerce:order_date'); ?>:
+      <b>
+        <?php echo date('d-n-Y', $order->time_updated);?>
+      </b>
+      <br />
+
+      <?php echo elgg_echo('izap-ecommerce:order_comfirmed'); ?>:
+      <b>
+        <?php echo $order->confirmed;?>
+      </b>
+      <br />
+
+      <?php echo elgg_echo('izap-ecommerce:payment_method'); ?>:
+      <b>
+        <?php 
+        if(isadminloggedin()) {
+          $method = ($order->payment_method) ? $order->payment_method : 'paypal';
+        }else{
+          $method = ($order->payment_method) ? $order->payment_method : 'paypal';
+          $info = func_get_custom_value_byizap(array(
+            'plugin' => GLOBAL_IZAP_PAYMENT_PLUGIN,
+            'var' => 'gateways_info',
+          ));
+
+          $method = $info[$method]['title'];
+        }
+        echo $method;
+        ?>
+      </b>
+      <br />
+
+    </div>
+
+  </div>
+  <div class="clearfloat"></div>
+</div>
+
+<?php
+echo elgg_view_title(elgg_echo('izap-ecommerce:order_details'));
 ?>
 <div class="contentWrapper">
   <?php
@@ -25,16 +99,26 @@ $order = $vars['entity'];
     $item_price = 'item_price_' . $i;
     $item_guid = 'item_guid_' . $i;
     $class = ($odd_even%2 == 0) ? 'even' : 'odd';
+
+    $product = get_entity($order->$item_guid);
+    if($product) {
+
+      $product_url = $product->getURL();
+    }else {
+      $product_url = '#';
+    }
     ?>
   <div class="izap-order-detait-<?php echo $class;?>">
     <div class="izap-product-float-left" style="width: 55%">
-        <?php echo $order->$item_name ?>
+      <a href="<?php echo $product_url?>">
+          <?php echo $order->$item_name ?>
+      </a>
     </div>
 
     <div class="izap-product-float-left" style="width: 20%">
-         <a href="<?php echo create_product_download_link_izap_ecommerce($order, $order->$item_guid)?>">
-           <?php _e('download');?>
-        </a>
+      <a href="<?php echo create_product_download_link_izap_ecommerce($order, $order->$item_guid)?>">
+          <?php _e('download');?>
+      </a>
     </div>
 
     <div class="izap-product-float-right" style="width: 20%">
