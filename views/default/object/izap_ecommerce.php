eb7ba26b503fcb5a73c25ba834de27db1d7ab3b7
Carl <carl@izap.in>
2001-10-03 23:22:25 +0000

    nthing much

--- views/default/object/izap_ecommerce.php
+++ views/default/object/izap_ecommerce.php
@@ -11,7 +11,7 @@
 * For more information. Contact "Tarun Jangra<tarun@izap.in>"
 * For discussion about corresponding plugins, visit http://www.pluginlotto.com/pg/forums/
 * Follow us on http://facebook.com/PluginLotto and http://twitter.com/PluginLotto
-*/
+ */
 
 global $IZAP_ECOMMERCE;
 if(get_input('search_viewtype') == 'gallery') {
@@ -22,22 +22,22 @@ if(get_input('search_viewtype') == 'gallery') {
   if(elgg_view_exists('output/entity_row')) {
 
     if($vars['entity']->getPrice(FALSE)) {
-    $extra .=  '<b>' . 
-      ((isloggedin()) ? elgg_echo('izap-ecommerce:price') : elgg_echo('izap-ecommerce:price_not_more'))
-      . '</b>: ' . $vars['entity']->getPrice() . '<br />';
-    }else{
+      $extra .=  '<b>' .
+              ((isloggedin()) ? elgg_echo('izap-ecommerce:price') : elgg_echo('izap-ecommerce:price_not_more'))
+              . '</b>: ' . $vars['entity']->getPrice() . '<br />';
+    }else {
       $extra .=  '<b>' . elgg_echo('izap-ecommerce:free') . '</b><br />';
     }
 
     if(IzapEcommerce::isInWishlist($vars['entity']->guid)) {
       $extra .= '<a href="'.
-      elgg_add_action_tokens_to_url(
+              elgg_add_action_tokens_to_url(
               func_get_actions_path_byizap(array(
-                'plugin' => GLOBAL_IZAP_ECOMMERCE_PLUGIN)) .
-                'remove_from_wishlist?guid=' . $vars['entity']->guid)
-      .'">'.elgg_echo('izap-ecommerce:remove_from_wishlist').'</a><br />';
+              'plugin' => GLOBAL_IZAP_ECOMMERCE_PLUGIN)) .
+              'remove_from_wishlist?guid=' . $vars['entity']->guid)
+              .'">'.elgg_echo('izap-ecommerce:remove_from_wishlist').'</a><br />';
     }
-    
+
     $extra .= elgg_view($IZAP_ECOMMERCE->product . 'edit_delete', array('entity' => $vars['entity']));
     echo elgg_view('output/entity_row', array('entity' => $vars['entity'], 'extra' => $extra));
   }else {
