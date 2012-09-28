52af2eb89aa9e782b53f46ff67679c82d273fb7a
Sona Kapoor <sona.kapoor@izap.in>
2012-09-28 17:21:45 +0530

    izap-ecommerce discount prize

--- views/default/izap-ecommerce/views/product/buy_options/add_to_cart.php
+++ views/default/izap-ecommerce/views/product/buy_options/add_to_cart.php
@@ -12,10 +12,10 @@
  * Follow us on http://facebook.com/PluginLotto and http://twitter.com/PluginLotto
  */
 
-$izap_eco = new IzapEcommerce();
-$izap_eco->hasUserPurchasedOldVersion(elgg_get_logged_in_user_entity());
-c($izap_eco);
 $product = elgg_extract('entity', $vars);
+$yes = $product->hasUserPurchasedOldVersion(elgg_get_logged_in_user_entity());
+c($yes);
+
 if( $product->comming_soon == 'no' && $product->getPrice(FALSE)>0)
 define('show_buy_now', 'yes');
 $color =$product->getPrice(FALSE) <= 0 ? '#4DD18C': '#4690D6';
