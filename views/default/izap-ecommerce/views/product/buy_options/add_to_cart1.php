c243b970947ed4a80ac86276e32d8955382a6b7c
Sona Kapoor <sona.kapoor@izap.in>
2012-10-01 19:39:00 +0530

    izap-ecommerce

--- views/default/izap-ecommerce/views/product/buy_options/add_to_cart1.php
+++ views/default/izap-ecommerce/views/product/buy_options/add_to_cart1.php
@@ -26,7 +26,7 @@ if ($product->getPrice(FALSE) <= 0) {
 }
 ?>
 <?php
-if ($yes && $vars[entity]->discount >0) : ?>
+if ($yes && $vars['entity']->discount >0) : ?>
   <div  class="price izap-line" id="discount_price_span " style="background-color:<?php echo ($yes) ? 'red' : $color ?>; float:none ">
     <?php echo elgg_echo('Actual Prize:$'); ?>
     <?php echo '<span id="product_price_html">' . $product->price . '</span>' ?>
