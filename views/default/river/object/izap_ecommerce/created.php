11d118ee7859b474874bbbbbe2a929f417684b13
Sona Kapoor <sona.kapoor@izap.in>
2012-10-01 19:33:01 +0530

    izap-ecommerce

--- views/default/river/object/izap_ecommerce/created.php
+++ views/default/river/object/izap_ecommerce/created.php
@@ -12,13 +12,13 @@
 * For discussion about corresponding plugins, visit http://www.pluginlotto.com/pg/forums/
 * Follow us on http://facebook.com/PluginLotto and http://twitter.com/PluginLotto
 */
-echo "sona";
+
 $performed_by = get_entity($vars['item']->subject_guid); // $statement->getSubject();
 $object = get_entity($vars['item']->object_guid);
 
 $url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
 $contents = strip_tags($object->description); //strip tags from the contents to stop large images etc blowing out the river view
-echo elgg_view('izap-ecommerce/views/product/buy_options/add_to_cart1',array('entity' => $object));
+echo elgg_view('izap-ecommerce/views/product/buy_options/add_to_cart1',array('entity' => $object)). "test";exit; 
 $string = $url . ' has '.$vars['item']->action_type.' '  . elgg_echo('item:' . $object->getType() . ':' . $object->getSubtype() . ':singular') . ' ';
 $string .= " <a href=\"" . $object->getURL() . "\">" . $object->title . "</a>";
 $string .= "<div class=\"river_content_display\">";
