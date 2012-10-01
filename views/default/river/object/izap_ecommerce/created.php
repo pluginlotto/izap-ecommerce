98bd9630c044424dd762aa5fccbff4349fdd023a
Sona Kapoor <sona.kapoor@izap.in>
2012-10-01 19:35:46 +0530

    izap-ecommerce

--- views/default/river/object/izap_ecommerce/created.php
+++ views/default/river/object/izap_ecommerce/created.php
@@ -18,7 +18,7 @@ $object = get_entity($vars['item']->object_guid);
 
 $url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
 $contents = strip_tags($object->description); //strip tags from the contents to stop large images etc blowing out the river view
-echo elgg_view('izap-ecommerce/views/product/buy_options/add_to_cart1',array('entity' => $object)). "test";exit; 
+
 $string = $url . ' has '.$vars['item']->action_type.' '  . elgg_echo('item:' . $object->getType() . ':' . $object->getSubtype() . ':singular') . ' ';
 $string .= " <a href=\"" . $object->getURL() . "\">" . $object->title . "</a>";
 $string .= "<div class=\"river_content_display\">";
@@ -28,5 +28,6 @@ if(strlen($contents) > 200) {
 }else {
   $string .= $contents;
 }
+$string .= elgg_view('izap-ecommerce/views/product/buy_options/add_to_cart1',array('entity' => $object)); 
 $string .= "</div><div class=\"clearfloat\"></div>";
 echo $string;
