6d58a35e6c72c77bef44cdd20270f596955003f0
Sona Kapoor <sona.kapoor@izap.in>
2012-10-01 13:24:44 +0530

    izap-ecommerce price view

--- views/default/izap-ecommerce/elements/summary.php
+++ views/default/izap-ecommerce/elements/summary.php
@@ -33,8 +33,9 @@ if ($title_link === '') {
 	$title_link = elgg_view('output/url', $params);
 }
 
-if($vars['is_available'])
+if($vars['is_available']) {
   $price_tag = sprintf('<div class="lprice" style="background-color:%s">%s</div>',$entity->getPrice(FALSE) ? "#4690D6" : "#4DD18C", elgg_echo('izap-ecommerce:price') . $entity->getPrice());
+  $discounted_price = elgg_view('izap-ecommerce/views/product/buy_options/add_to_cart1',$vars);}
 else
   $price_tag = sprintf('<div class="lcomingsoon">%s</div>', elgg_echo('izap-ecommerce:comming_soon'));
 
@@ -53,6 +54,7 @@ if ($metadata) {
 echo "<h3>$title_link</h3>";
 echo '<br/>';
 echo $price_tag;
+echo $discounted_price;
 echo "<div class=\"elgg-subtext\">$subtitle</div>";
 echo $tags;
 if ($content) {
