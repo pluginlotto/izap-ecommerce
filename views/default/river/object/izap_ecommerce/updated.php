8b1615da794685aabd2474dccb644e7731ea2f21
Sona Kapoor <sona.kapoor@izap.in>
2012-10-04 13:11:11 +0530

    css changes

--- views/default/river/object/izap_ecommerce/updated.php
+++ views/default/river/object/izap_ecommerce/updated.php
@@ -29,7 +29,7 @@ if(strlen($contents) > 200) {
   $string .= $contents;
 }
 $string .= '<div style="float:right">'.elgg_view('izap-ecommerce/views/product/buy_options/add_to_cart1',array('entity' => $object)).'</div> </br> </br></br>'; 
-$string .= '<br/><div style="float:right">'.elgg_view('izap-ecommerce/views/product/buy_options/price',array('entity' => $object)).'</div> '; 
+$string .= '<div style="float:right">'.elgg_view('izap-ecommerce/views/product/buy_options/price',array('entity' => $object)).'</div><br/>'; 
 $string .= "</div><div class=\"clearfloat\"></div>";
 echo $string;
 
