ee14e02a014e9c64522b170186bde5963482d396
Sona Kapoor <sona.kapoor@izap.in>
2012-10-04 12:58:02 +0530

    izap-ecommerce

--- views/default/izap-ecommerce/views/product/buy_options/add_to_cart.php
+++ views/default/izap-ecommerce/views/product/buy_options/add_to_cart.php
@@ -19,22 +19,26 @@ if ($product->comming_soon == 'no' && $product->getPrice(FALSE) > 0)
   define('show_buy_now', 'yes');
 //$color = $product->getPrice(FALSE) <= 0 ? '#4DD18C': '#4690D6';
 if ($product->getPrice(FALSE) <= 0) {
-   $color = '#4DD18C';
+  $color = '#4DD18C';
 } elseif ($product->getPrice(FALSE) <= 0 && $yes == 1) {
-   $color = 'red';
+  $color = 'red';
 } else {
-   $color = '#4690D6';
+  $color = '#4690D6';
 }
-if ($yes) : ?>
-<div>
-  <div style="float:left">
- <?php echo elgg_view('izap-ecommerce/views/product/buy_options/add_to_cart1',$vars); ?>
-  <?php endif ?>
+if ($yes) :
+  ?>
+  <div>
+    <div style="float:left">
+      <?php echo elgg_view('izap-ecommerce/views/product/buy_options/add_to_cart1', $vars); ?>
+<?php endif ?>
   </div>
 <!--<div class="price" id="price_span" style="background-color:<?php echo $color ?>; float:left;">
-    <?php //echo elgg_echo('izap-ecommerce:price'); ?>
-    <?php //echo '<span id="product_price_html">' . $product->getPrice() . '</span>' ?>
+  <?php //echo elgg_echo('izap-ecommerce:price'); ?>
+<?php //echo '<span id="product_price_html">' . $product->getPrice() . '</span>'  ?>
   </div>-->
-  <?php echo elgg_view('izap-ecommerce/views/product/buy_options/price',$vars); ?>
+    <div class="clearfloat"></div>
+  <div style="float:left">
+<?php echo elgg_view('izap-ecommerce/views/product/buy_options/price', $vars); ?>
+  </div>
   <div class="clearfloat"></div>
 </div> 
