833f3a93bca801c8baa6335f7fcea16673729826
Sona Kapoor <sona.kapoor@izap.in>
2012-10-04 11:47:23 +0530

    izap-ecommerce

--- views/default/izap-ecommerce/views/product/buy_options/add_to_cart.php
+++ views/default/izap-ecommerce/views/product/buy_options/add_to_cart.php
@@ -14,7 +14,6 @@
  */
 
 $product = elgg_extract('entity', $vars);
-echo $vars['full_view'];
 $yes = $product->hasUserPurchasedOldVersion(elgg_get_logged_in_user_entity());
 if ($product->comming_soon == 'no' && $product->getPrice(FALSE) > 0)
   define('show_buy_now', 'yes');
